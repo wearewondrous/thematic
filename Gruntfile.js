a678a37d39d82c06cbe4c590fe1c927940b1fd5a
pheadeaux <pheadeaux@gmail.com>
2016-06-23 22:25:28 +0200

    grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
           expand: true,
           cwd: 'sass',
           src: ['*.scss'],
-          dest: '../sass',
+          dest: 'css',
           ext: '.css'
         }]
       }
